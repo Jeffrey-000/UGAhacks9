980ffbc186a37f79cca3eb099f3a79d047cca313
Jeffaaaaaah <jeffaaaaaah@gmail.com>
2024-02-10 00:28:55 -0500

    springboot project init

---
+++ HHID/src/test/java/dad/imhungry/HHID/HhidApplicationTests.java
@@ -0,0 +1,13 @@
+package dad.imhungry.HHID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
